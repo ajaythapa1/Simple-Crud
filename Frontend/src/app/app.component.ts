d4eaf07e1f28af4f1b2568678621ab129f55b8d0
ajaythapa1 <ajaythapa574@gmail.com>
2025-02-27 11:34:45 +0545

    add api link

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,14 +1,28 @@
 // parent.component.ts
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormComponent } from './Form/form/form.component';
+import { HttpClientModule } from '@angular/common/http';
+// import { FormService } from './shared/form.service';
+
 
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [FormComponent]
+  imports: [FormComponent, HttpClientModule]
 
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  // constructor(private formservice: FormService) {
+  // }
+  ngOnInit(): void {
+    // this.formservice.refreshList().subscribe({
+    //   next: (res) => console.log(res),
+    //   error: (err) => console.error(err)
+    // });
+
+  }
 }
--- Frontend/src/app/shared/form.service.ts
+++ Frontend/src/app/shared/form.service.ts
@@ -1,10 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment.development';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FormService {
+  url: string = environment.apiBaseUrl + '/Student'
 
   constructor(private http: HttpClient) { }
+  refreshlist() {
+    this.http.get(this.url)
+    .subscribe({
+      next: res => {
+        console.log(res);
+      },
+      error: err => {console.log(err)
+      }
+    })
+  }
 }
